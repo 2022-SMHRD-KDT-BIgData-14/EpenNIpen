a3f2321f5b505d5546d7401004a78e8baa116d3e
smhrd <smhrd@119.200.31.122>
2022-08-03 10:21:32 +0900

    [상익]_220803_1021 드림펜찾기 기능구현 및 페이지 연결

--- EpenNIpen/src/main/webapp/sql.sql
+++ EpenNIpen/src/main/webapp/sql.sql
@@ -11,4 +11,8 @@ where TYPE_1 in '만년필'
 select * from member_info
 
 
-select type_1 from PRODUCT_INFO where product='파버카스텔 TK 4600 클러치 펜슬 홀더 2mm'
+select type_1 from PRODUCT_INFO where product='펜텔 그래프1000 리미티드9 트로피컬 썸머 그라데이션'
+
+select * from question
+
+select * from(select * from question where id='111' order by q_seq desc) where rownum=1
